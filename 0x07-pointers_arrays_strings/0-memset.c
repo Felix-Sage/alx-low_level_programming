3229998b853401ed3c488f84a6b03a0caad9d712
felix-sage <felixtolawinjobi@gmail.com>
2023-01-02 00:07:03 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,15 @@
  * *@b: constant byte
  * *@n:number of byte
  * *Return: returns new value of target
- * */
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-		while (n)
-				{
-							s[n - 1] = b;
-									n--;
-										}
-			return (s);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 
 }
