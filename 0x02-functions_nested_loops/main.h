2899b08b3df449794728c431bda7e5fd316f794c
felix-sage <felixtolawinjobi@gmail.com>
2023-01-03 11:53:22 -0800

    task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ *  * File: main.h
+ *   * Auth: Brennan D Baraban
+ *    * Desc: Header file containing declarations for all functions
+ *     *       used in the 0x02-functions_nested_loops directory.
+*/
+
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
+
+#endif
